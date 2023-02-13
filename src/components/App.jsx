5e64f5036be6136358f900342e7658bce6b9eb3c
Alexander Repeta <alexander.repeta@gmail.com>
2023-02-13 19:55:55 +0200

    udpate text

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,10 +7,10 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      React homework template
+      React homework template1
     </div>
   );
 };
